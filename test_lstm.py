1fbe4a134f62e0cc31f327515dfa6291922f5781
ben <b.guy@archangel.im>
2019-03-28 09:04:20 +0000

    Added trained checkpoint and restructured code

--- test_lstm.py
+++ test_lstm.py
@@ -12,10 +12,12 @@ import collections
 import time
 
 # Global variables
-trainData = 'input_data.txt'
+trainData = 'test_input_data.txt'
 modelDir = 'models/'
-numInputs = 3
-numHidden = 512
+numInputs = 4
+numHidden = 128
+numRnnCells = 3
+keepProb = 1.0
 
 # Function to read the text data from a file
 def read_data(fname):
@@ -36,6 +38,23 @@ def build_dataset(words):
     reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
     return dictionary, reverse_dictionary
 
+# Function to create an RNN cell with dropout
+def createRNNCell():
+    rnnCell = rnn.BasicLSTMCell(numHidden)   
+    rnnDropout = rnn.DropoutWrapper(rnnCell,input_keep_prob=keepProb, output_keep_prob=keepProb)
+    return rnnDropout
+
+# Function to define the RNN structure
+def RNN(x, weights, biases):
+    x = tf.reshape(x, [-1, numInputs])
+    x = tf.split(x,numInputs,1)
+
+    rnnNet = rnn.MultiRNNCell([createRNNCell() for _ in range(numRnnCells)])
+
+    outputs, states = rnn.static_rnn(rnnNet, x, dtype=tf.float32)
+    return tf.matmul(outputs[-1], weights['out']) + biases['out']
+
+
 
 # Load up the training data
 print("Loading training data...")
@@ -52,34 +71,12 @@ y = tf.placeholder("float", [None, vocab_size])
 weights = {'out': tf.Variable(tf.random_normal([numHidden, vocab_size]))}
 biases = {'out': tf.Variable(tf.random_normal([vocab_size]))}
 
-# Define the RNN structure
-def RNN(x, weights, biases):
-    # reshape to [1, numInputs]
-    x = tf.reshape(x, [-1, numInputs])
-    # Generate a numInputs-element sequence of inputs
-    # (eg. [had] [a] [general] -> [20] [6] [33])
-    x = tf.split(x,numInputs,1)
-    # 2-layer LSTM, each layer has numHidden units.
-    # Average Accuracy= 95.20% at 50k iter
-    
-    rnn_cell = rnn.MultiRNNCell([rnn.BasicLSTMCell(numHidden),rnn.BasicLSTMCell(numHidden)])
-    # 1-layer LSTM with numHidden units but with lower accuracy.
-    # Average Accuracy= 90.60% 50k iter
-    # Uncomment line below to test but comment out the 2-layer rnn.MultiRNNCell above
-    # rnn_cell = rnn.BasicLSTMCell(numHidden)
-    # generate prediction
-    outputs, states = rnn.static_rnn(rnn_cell, x, dtype=tf.float32)
-    # there are numInputs outputs but
-    # we only want the last output
-    return tf.matmul(outputs[-1], weights['out']) + biases['out']
+# Create the RNN here
 pred = RNN(x, weights, biases)
 
 # Model evaluation
 correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-
-# Initializing the variables
-init = tf.global_variables_initializer()
 
 # Create the saver
 saver = tf.train.Saver(tf.trainable_variables())
